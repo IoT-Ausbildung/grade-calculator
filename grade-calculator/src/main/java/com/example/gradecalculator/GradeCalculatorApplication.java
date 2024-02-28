ee75e79e18e1400179f5ecb536d3311e257539db
Diana Mirlanbekova <diana.mirlanbekova@maibornwolff.de>
2024-02-28 09:56:03 +0100

    no message

--- grade-calculator/src/main/java/com/example/gradecalculator/GradeCalculatorApplication.java
+++ grade-calculator/src/main/java/com/example/gradecalculator/GradeCalculatorApplication.java
@@ -1,13 +1,14 @@
-package com.example.gradecalculator;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.yourpackage.repository")
+@EntityScan(basePackages = "com.yourpackage.entity")
 public class GradeCalculatorApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(GradeCalculatorApplication.class, args);
+		SpringApplication.run(YourApplication.class, args);
 	}
-
 }
+
