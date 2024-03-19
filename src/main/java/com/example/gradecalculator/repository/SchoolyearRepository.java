82c70462c7be71d74c28deb2be9d4e0efe7d0f01
Asman97 <148758652+Asman97@users.noreply.github.com>
2024-03-19 10:33:12 +0100

    .gitignore is now working

--- src/main/java/com/example/gradecalculator/repository/SchoolyearRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gradecalculator.repository;
-
-import com.example.gradecalculator.entities.SchoolYear;
-import org.springframework.data.repository.CrudRepository;
-public interface SchoolYearRepository extends CrudRepository<SchoolYear, Long> {
-
-    SchoolYear findById(long id);
-}
