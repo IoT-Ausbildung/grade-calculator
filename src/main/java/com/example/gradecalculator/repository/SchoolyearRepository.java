6d8c55db5491cd97693acc2984aebbf6f5153ef6
Asman97 <148758652+Asman97@users.noreply.github.com>
2024-03-19 09:33:02 +0100

    findBy was changed #4

--- src/main/java/com/example/gradecalculator/repository/SchoolyearRepository.java
+++ src/main/java/com/example/gradecalculator/repository/SchoolyearRepository.java
@@ -1,9 +1,10 @@
 package com.example.gradecalculator.repository;
 
-import com.example.gradecalculator.entities.GradeSystem;
 import com.example.gradecalculator.entities.SchoolYear;
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 public interface SchoolYearRepository extends CrudRepository<SchoolYear, Long> {
-    List<SchoolYear> findByGradeSystem(GradeSystem gradeSystem);
+
+
+
+    SchoolYear findById(long id);
 }
