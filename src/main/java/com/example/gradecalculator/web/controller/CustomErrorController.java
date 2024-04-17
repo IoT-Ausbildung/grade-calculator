9674b443dd9b146a836b7d572de5d981aeba0182
Louis Hüfner <louis.huefner@maibornwolff.de>
2024-04-17 10:48:18 +0200

    #24 Error Page: Pull Request review suggestions without dEisenbeiss (Switch Case)

--- src/main/java/com/example/gradecalculator/web/controller/CustomErrorController.java
+++ src/main/java/com/example/gradecalculator/web/controller/CustomErrorController.java
@@ -9,26 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyErrorController implements ErrorController {
+public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
+            Integer statusCode = (Integer) status;
 
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                model.addAttribute("message", "Error " + statusCode + ": Page not found");
+            switch (statusCode) {
+                case HttpStatus.NOT_FOUND.value():
+                    model.addAttribute("message", "Error " + statusCode + ": Page not found");
+                    break;
+                case HttpStatus.INTERNAL_SERVER_ERROR_VALUE:
+                    model.addAttribute("message", "Error " + statusCode + ": Internal server error");
+                    break;
+                default:
+                    model.addAttribute("message", "Error " + statusCode + ": An error occurred");
+                    break;
             }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                model.addAttribute("message", "Error " + statusCode + ": Internal server error");
-            }
-            else {
-                model.addAttribute("message", "Error " + statusCode + ": An error occurred");
-            }
-        }
-        else {
+        } else {
             model.addAttribute("message", "An unexpected error occurred");
         }
         return "error";
