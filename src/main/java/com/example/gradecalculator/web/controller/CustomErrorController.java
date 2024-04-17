60a44e20583fb7aa21761d068fe56ccf9639e1a4
Louis Hüfner <louis.huefner@maibornwolff.de>
2024-04-17 14:18:38 +0200

    #24 Error Page: Pull Request review suggestions "dEisenbeiss Switch Case"

--- src/main/java/com/example/gradecalculator/web/controller/CustomErrorController.java
+++ src/main/java/com/example/gradecalculator/web/controller/CustomErrorController.java
@@ -1,37 +1,42 @@
-//package com.example.gradecalculator.web.controller;
-//
-//import jakarta.servlet.RequestDispatcher;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class CustomErrorController implements ErrorController {
-//
-//    @RequestMapping("/error")
-//    public String handleError(HttpServletRequest request, Model model) {
-//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//
-//        if (status != null) {
-//            Integer statusCode = (Integer) status;
-//
-//            switch (statusCode) {
-//                case HttpStatus.NOT_FOUND.value():
-//                    model.addAttribute("message", "Error " + statusCode + ": Page not found");
-//                    break;
-//                case HttpStatus.INTERNAL_SERVER_ERROR_VALUE:
-//                    model.addAttribute("message", "Error " + statusCode + ": Internal server error");
-//                    break;
-//                default:
-//                    model.addAttribute("message", "Error " + statusCode + ": An error occurred");
-//                    break;
-//            }
-//        } else {
-//            model.addAttribute("message", "An unexpected error occurred");
-//        }
-//        return "error";
-//    }
-//}
+package com.example.gradecalculator.web.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String errormessage = "An unexpected error occurred";
+        String error = "";
+
+        if (status != null) {
+            //HttpStatus statusCode = (HttpStatus) status;
+            Integer statusCode = Integer.valueOf(status.toString());
+            switch (statusCode) {
+                case 404:
+                    errormessage = ": Page not found";
+                    error = "Error";
+                    break;
+                case 500:
+                    errormessage = ": Internal server error";
+                    error = "Error";
+                    break;
+                default:
+                    errormessage = ": Unknown error occurred";
+                    break;
+            }
+        }else {
+            status = "";
+        }
+        model.addAttribute("message", error +" "+ status + errormessage);
+        return "error";
+    }
+}
