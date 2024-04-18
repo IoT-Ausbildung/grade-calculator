cab3eefac549b9141f84919173aa5151df0e1b82
Louis Hüfner <louis.huefner@maibornwolff.de>
2024-04-18 10:53:44 +0200

    #24 Error Page: Pull Request review suggestions "dEisenbeiss Switch case"

--- src/main/java/com/example/gradecalculator/web/controller/CustomErrorController.java
+++ src/main/java/com/example/gradecalculator/web/controller/CustomErrorController.java
@@ -14,23 +14,21 @@ public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-            Integer statusCode = (Integer) status;
-            switch (statusCode) {
-                case 404:
-                    model.addAttribute("message", "Error " + statusCode + ": Page not found");
-                    break;
-                case 500:
-                    model.addAttribute("message", "Error " + statusCode + ": Internal server error");
-                    break;
-                default:
-                    model.addAttribute("message", "Error " + statusCode + ": An error occurred");
-                    break;
-            }
-        } else {
-            model.addAttribute("message", "An unexpected error occurred");
-        }
+        String errorMessage = getErrorMessage(status);
+        model.addAttribute("message", errorMessage);
         return "error";
+    }
+
+    private static String getErrorMessage(Object status) {
+        if (status == null) {
+            return "An unexpected error occurred";
+        }
+        Integer statusCode = (Integer) status;
+        String statusMessage = switch (statusCode) {
+            case 404 -> "Page not found";
+            case 500 -> "Internal server error";
+            default -> "An error occurred";
+        };
+        return "Error " + statusCode + ": " + statusMessage;
     }
 }
