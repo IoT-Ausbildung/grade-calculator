33fd6a71b9216d962c45f2b35a0ff753db981630
Louis Hüfner <louis.huefner@maibornwolff.de>
2024-04-18 10:22:49 +0200

    #24 Error Page: Pull Request review suggestions "dinowol filename Small and 'password', 'passwordCf' as constans"

--- src/main/resources/static/passwordValidation.js
+++ src/main/resources/static/passwordValidation.js
@@ -1,18 +1,21 @@
+const passwordId = "password";
+const passwordCfId = "passwordConfirm";
+
 function validatePassword(){
-    var passwordIn = document.getElementById("password");
-    var passwordCf = document.getElementById("passwordConfirm");
+    var passwordIn = document.getElementById(passwordId);
+    var passwordCf = document.getElementById(passwordCfId);
     if(passwordIn.value != passwordCf.value) {
         passwordCf.setCustomValidity("Passwords Don't Match");
     } else {
         passwordCf.setCustomValidity('');
     }
 }
-document.getElementById("password").onchange = validatePassword;
-document.getElementById("passwordConfirm").onkeyup = validatePassword;
+document.getElementById(passwordId).onchange = validatePassword;
+document.getElementById(passwordCfId).onkeyup = validatePassword;
 
 function togglePasswordVisibility() {
-    var passwordField = document.getElementById("password");
-    var passwordConfirmField = document.getElementById("passwordConfirm");
+    var passwordField = document.getElementById(passwordId);
+    var passwordConfirmField = document.getElementById(passwordCfId);
 
     toggleInputVisibility(passwordField);
     toggleInputVisibility(passwordConfirmField);
