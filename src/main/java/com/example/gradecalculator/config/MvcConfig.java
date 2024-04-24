68521414f12fa6a5dd1b1654dcc4319af02f061b
Louis Hüfner <louis.huefner@maibornwolff.de>
2024-04-24 11:30:50 +0200

    #29-Ordnerstruktur "Pull-Request"

--- src/main/java/com/example/gradecalculator/config/MvcConfig.java
+++ src/main/java/com/example/gradecalculator/config/MvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/").setViewName("redirect:html/index.html");
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
