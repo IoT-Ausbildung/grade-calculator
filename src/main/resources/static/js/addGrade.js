330b7c6e68b61d143fca0a1befc23defc271df52
Asman97 <148758652+Asman97@users.noreply.github.com>
2024-10-24 11:49:22 +0200

    #45 disabling addGradeButton when nothing has been chosen and replacing div with row

--- src/main/resources/static/js/addGrade.js
+++ src/main/resources/static/js/addGrade.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
-    $('.add-grade-btn').click(function () {
+    const addGradeButton = $('.add-grade-btn');
+    addGradeButton.prop('disabled', true);
+
+    function validateGradeValue(gradeValue) {
+        return gradeValue >= 1 && gradeValue <= 6;
+    }
+
+    $('input[name="grade"]').on('input', function () {
+        const gradeValue = $(this).val();
+
+        if (validateGradeValue(gradeValue)) {
+            addGradeButton.prop('disabled', false);
+        } else {
+            addGradeButton.prop('disabled', true);
+        }
+    });
+
+    addGradeButton.click(function () {
         const gradeInput = $(this).closest('tr').find('input[name="grade"]');
         const gradeTypeSelect = $(this).closest('tr').find('select[name="gradeType"]');
         const userSubjectId = $(this).closest('tr').find('.remove-btn').data('id');
         const gradeValue = gradeInput.val();
         const gradeTypeId = gradeTypeSelect.val();
         const gradeTypeName = gradeTypeSelect.find("option:selected").text();
-        if (!gradeValue || gradeValue < 1 || gradeValue > 6) {
+
+        if (!validateGradeValue(gradeValue)) {
             showAlertModal('Please select a valid grade between 1 and 6.');
             return;
         }
@@ -33,7 +51,10 @@ $(document).ready(function () {
             contentType: 'application/json',
             data: JSON.stringify(gradeData),
             beforeSend: function (xhr) {
-                xhr.setRequestHeader($('meta[name="_csrf_header"]').attr('content'), $('meta[name="_csrf"]').attr('content'));
+                xhr.setRequestHeader(
+                    $('meta[name="_csrf_header"]').attr('content'),
+                    $('meta[name="_csrf"]').attr('content')
+                );
             },
             success: function () {
                 window.location.reload();
@@ -43,5 +64,4 @@ $(document).ready(function () {
             }
         });
     }
-
 });
